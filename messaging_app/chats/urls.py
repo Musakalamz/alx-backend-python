7294a367acde77760f27407f74fffbc3d74d449a
Musakalamz <ogunsolumusa@gmail.com>
2025-11-14 11:53:03 +0100

    URLs: register DefaultRouter and NestedDefaultRouter; include /api/ routes and api-auth

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
+router = DefaultRouter()  # routers.DefaultRouter()
 router.register('conversations', ConversationViewSet, basename='conversation')
 router.register('messages', MessageViewSet, basename='message')
 
+nested_router = routers.NestedDefaultRouter(router, 'conversations', lookup='conversation')  # NestedDefaultRouter
+nested_router.register('messages', MessageViewSet, basename='conversation-messages')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(nested_router.urls)),
 ]
