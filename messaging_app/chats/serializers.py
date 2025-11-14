5b213ce2839aada669882239d44c8323c6608a04
Musakalamz <ogunsolumusa@gmail.com>
2025-11-14 10:33:50 +0100

    Add serializers for User, Conversation, and Message with nested relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 from rest_framework import serializers
-from .models import Message
+from .models import User, Message, Conversation
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'phone_number', 'role', 'created_at']
+
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
     class Meta:
         model = Message
-        fields = ['id', 'sender', 'content', 'created_at']
+        fields = ['id', 'sender', 'conversation', 'message_body', 'sent_at']
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['id', 'participants', 'messages', 'created_at']
